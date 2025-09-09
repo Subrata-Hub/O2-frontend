5d9f5e52b551499216f73a6c15ce9078902a217f
Subrata Guchhait <sguchhaitdev@gmail.com>
2025-09-10 00:15:46 +0530

    Changing port

--- src/lib/functions.ts
+++ src/lib/functions.ts
@@ -7,7 +7,7 @@ interface UserData {
 }
 
 export async function login(email: string, password: string) {
-  const response = await fetch("http://localhost:3000/api/auth/login", {
+  const response = await fetch("http://localhost:7777/api/auth/login", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
@@ -21,7 +21,7 @@ export async function login(email: string, password: string) {
 }
 
 export async function signup(userData: UserData) {
-  const response = await fetch("http://localhost:3000/api/auth/signup", {
+  const response = await fetch("http://localhost:7777/api/auth/signup", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
